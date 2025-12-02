c2f37e7e46a0f8f8fa40c9216dfac3f985802c7a
ranasaad0 <saadtalha40@gmail.com>
2025-12-02 09:53:53 +0500

    Create datacleaning.py

---
+++ datacleaning.py
@@ -0,0 +1,6 @@
+import pandas as pd
+#load data (example: from a URL or a local file)
+url="https://people.sc.fsu.edu/~jburkadt/data/csv/hw_200.csv"
+df = pd.read_csv(url)
+#cleaning example
+df.dropn
